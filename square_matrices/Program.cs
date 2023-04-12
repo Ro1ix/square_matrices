9a772b4488924efad774c47b0d19eadfd8ac2e10
Rolix <130452450+Ro1ix@users.noreply.github.com>
2023-04-12 10:41:48 +0300

    v0.1
    Вообще не готово

---
+++ square_matrices/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ввдедите размер квадратных матриц");
+int N = Convert.ToInt32(Console.ReadLine());
+string size = "";
+for (int i = 1; i < N; i++)
+{
+    size = size + ",";
+}
+Console.WriteLine(size);
